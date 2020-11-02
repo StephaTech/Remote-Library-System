11e8db9e7849f77474220d94b3869d95ebba3190
StephaTech <stephatech123@gmail.com>
2020-11-02 00:14:48 +0000

    TextFile, class

--- src/Book.java
+++ src/Book.java
@@ -1,4 +1,30 @@
 
 public class Book {
+	private int IntStandBookNumber;
+	private String title, authorName;
+	
+	
+	public Book() {
+		
+		this.IntStandBookNumber = IntStandBookNumber;
+		this.title = title;
+		this.authorName = authorName;
+	}
 
+
+	public Book(int id, String title, String author) {
+		super();
+		this.IntStandBookNumber = id;
+		this.title = title;
+		this.authorName = author;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Book [id=" + IntStandBookNumber + ", title=" + title + ", author=" + authorName + "]";
+	}
+	
+	
+	
 }
