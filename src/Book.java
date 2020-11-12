718055b2bbc7f8f339d8984c669cf069beb69795
StephaTech <stephatech123@gmail.com>
2020-11-12 19:58:12 +0000

    Load Data Book

--- src/Book.java
+++ src/Book.java
@@ -1,30 +1,60 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Book {
-	private int IntStandBookNumber;
-	private String title, authorName;
-	
-	
-	public Book() {
-		
-		this.IntStandBookNumber = IntStandBookNumber;
-		this.title = title;
-		this.authorName = authorName;
-	}
 
+    private long isbn;//long because the number is long
+    private String title, authorName;
+    final ArrayList<Book> records = new ArrayList<>();
 
-	public Book(int id, String title, String author) {
-		super();
-		this.IntStandBookNumber = id;
-		this.title = title;
-		this.authorName = author;
-	}
+    public Book(final long isbn, final String title, final String authorName) {
 
+        this.isbn = isbn;
+        this.title = title;
+        this.authorName = authorName;
+    }
 
-	@Override
-	public String toString() {
-		return "Book [ISBN:" + IntStandBookNumber + ", Title:" + title + ", Author:" + authorName + "]";
-	}
-	
-	
-	
+    public Book(){//constructor without parameter
+
+    }
+
+    public ArrayList<Book> loadDataBook() {
+
+        try {
+            final BufferedReader br = new BufferedReader(new FileReader("BookInformation.txt"));
+            String contentLine = br.readLine();
+
+            String[] data;
+            long isbn;
+            final String name;
+            String authorName;
+
+            while (contentLine != null) {
+
+                data = contentLine.split(",");
+                isbn = Long.parseLong(data[0]);//converting for int
+                title = data[1];
+                authorName = data[2];
+                final Book book = new Book(isbn, title, authorName);
+                records.add(book);
+                contentLine = br.readLine();
+                System.out.println(book.toString());
+            }
+
+        } catch (final IOException ex) {
+            Logger.getLogger(Book.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        return records;
+    }
+
+    @Override
+    public String toString() {
+        return "Book [ISBN:" + isbn + ", Title:" + title + ", Author:" + authorName + "]";
+    }
+
 }
--- src/CmdLine.java
+++ src/CmdLine.java
@@ -1,20 +1,29 @@
 
 public class CmdLine {
-	public CmdLine() {
-		System.out.println("********************************************************************\n"
-				+ "*              Welcome to the Remote Library System 			\n"
-				+ "********************************************************************\n"
-				+ "* (1) Search for a book by title and/or author name 			\n"
-				+ "* (2) List all books by title an/or author name alphabetical order \n"
-				+ "* (3) Search for a specific reader by name and/ or ID			\n"
-				+ "* (4) List all readers by alphabetical and/or ID order		\n"
-				+ "* (5) Registered that reader has borrowed a book				\n"
-				+ "* (6) Add readers to a waiting list(queue)					\n"
-				+ "* (7) Register readers return book 							\n"
-				+ "* (8) List next reader for that book 							\n"
-				+ "* (9) Reader: list the books that they have borrowed 			\n"
-				+ "* (0) Exit library											\n"
-				+ "***************************************************************\n");
+    
+    public CmdLine() {
+
+        
+        Book book = new Book();
+        book.loadDataBook();
+
+
+		// System.out.println("********************************************************************\n"
+		// 		+ "*              Welcome to the Remote Library System 			\n"
+		// 		+ "********************************************************************\n"
+		// 		+ "* (1) Search for a book by title and/or author name 			\n"
+		// 		+ "* (2) List all books by title an/or author name alphabetical order \n"
+		// 		+ "* (3) Search for a specific reader by name and/ or ID			\n"
+		// 		+ "* (4) List all readers by alphabetical and/or ID order		\n"
+		// 		+ "* (5) Registered that reader has borrowed a book				\n"
+		// 		+ "* (6) Add readers to a waiting list(queue)					\n"
+		// 		+ "* (7) Register readers return book 							\n"
+		// 		+ "* (8) List next reader for that book 							\n"
+		// 		+ "* (9) Reader: list the books that they have borrowed 			\n"
+		// 		+ "* (0) Exit library											\n"
+		// 		+ "***************************************************************\n");
 	}
+
+
 
 }
