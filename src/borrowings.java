bd31a2fb2cf8a245739386d8c8a8c54e6247c043
StephaTech <stephatech123@gmail.com>
2020-12-03 18:12:51 +0000

    update

--- src/borrowings.java
+++ src/borrowings.java
@@ -1,4 +1,9 @@
+import java.util.ArrayList;
 
-public class borrowings {
+public class borrowings extends Reader {
+	ArrayList<Reader> readers = new ArrayList<>();
+	ArrayList<Book> books =  new ArrayList<>();
+	//ArrayList<Borrowing> borrowings = new ArrayList<>();
+	
 
 }
--- src/waitingList.java
+++ src/waitingList.java
@@ -2,4 +2,5 @@
 public class waitingList {
 	private String bookQueue;
 	private String readerQueue;
+	private String status;//this atribute should can stay here or should be at class book?
 }
